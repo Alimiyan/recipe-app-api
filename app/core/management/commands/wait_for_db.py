8102b3f8f2d4eb1553ba84b23741dd352d990fad
Alimiyan <alimiyan123@gmail.com>
2023-12-24 16:57:20 +0530

    implemented postgresql

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,23 @@
+# django command to wait for db to be available before starting the django app
+
+import time
+from psycopg2 import OperationalError as OpError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    # django command to wait for db
+
+    def handle(self, *args, **options):
+        # handle the command
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (OpError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
